71195cf8e8407dbca6859b265c60fa4e47f92205
Akhil Augustin <akhiljw46@gmail.com>
2023-02-05 22:58:40 +0530

    Tested sample data into database

--- server.js
+++ server.js
@@ -32,6 +32,20 @@ const tourSchema = new mongoose.Schema({
   },
 });
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  // rating: 4.7,
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log('ERROR💥💥💥:', err));
+
 const app = require('./app');
 
 // console.log(process.env);
