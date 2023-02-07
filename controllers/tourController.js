3bbcc50574a1fe0211ecd450be685d3c97ee0b80
Akhil Augustin <akhiljw46@gmail.com>
2023-02-07 07:45:21 +0530

    Functionality added: Limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,14 @@ exports.getAllTours = async (req, res) => {
       query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // Execute query
     const tours = await query;
     // const query = await Tour.find()
