1dc2a6f2cc1b2a2c9224851572264f2a60197272
Akhil Augustin <akhiljw46@gmail.com>
2023-02-11 01:28:20 +0530

    Handled uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting Down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -27,8 +33,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! 💥 Shutting Down...');
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
