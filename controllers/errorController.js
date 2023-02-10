90f402ddc384fb58141bd3da0bf21e0ac2ef475b
Akhil Augustin <akhiljw46@gmail.com>
2023-02-10 07:43:59 +0530

    Handled duplicate fields error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,14 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  //   const value = err.message.match(/(["'])(\\?.)*?\1/);
+  const value = err.keyValue.name;
+  // console.log(value, 'Hi alihi');
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -36,7 +44,7 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  //   console.log(err.stack);
+  // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
@@ -46,6 +54,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
