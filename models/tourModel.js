97796d5891a70f37d9d5a53ba60704b0d570c8f8
Akhil Augustin <akhiljw46@gmail.com>
2023-02-15 06:57:09 +0530

    Modelled tour guides and embedded in tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const user = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -102,6 +103,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +118,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 //Document Middleware: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await user.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
