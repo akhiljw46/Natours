cc0a1fef23239ddbdc199f5cadb32f525754a9ea
Akhil Augustin <akhiljw46@gmail.com>
2023-02-20 07:48:42 +0530

    Used mixins in pug to fill first part of tour details page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,3 +1,4 @@
+const { $where } = require('../models/tourModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -15,8 +16,18 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  // 2) Build template
+
+  // Render template from data from 1
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
