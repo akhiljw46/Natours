cf1e7d7b543e42261044d925228a107cdeec71f0
Akhil Augustin <akhiljw46@gmail.com>
2023-02-21 08:07:53 +0530

    Feature added: Displaying logged in user image and name

--- controllers/authController.js
+++ controllers/authController.js
@@ -199,3 +199,26 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // 4) Log user in, send JWT
   createSendToken(user, 200, res);
 });
+
+// Only for rendered pages, no errors
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1) Verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+
+    // 2) Check if usr still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) return next();
+
+    // 3) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) return next();
+
+    // Thre is a logged in user
+    res.locals.user = currentUser;
+    return next();
+  }
+  next();
+});
--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,6 @@
 
 const login = async (email, password) => {
   try {
-    console.log(email, password);
     const res = await axios({
       method: 'POST',
       url: 'http://127.0.0.1:3000/api/v1/users/login',
@@ -11,9 +10,15 @@ const login = async (email, password) => {
         password,
       },
     });
-    console.log(res);
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data);
+    alert(err.response.data.message);
   }
 };
 
