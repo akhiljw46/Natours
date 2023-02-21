ae8e9f680eca0f0001b0d8c359b28b7909fd1724
Akhil Augustin <akhiljw46@gmail.com>
2023-02-21 21:17:40 +0530

    Log out functionality added

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,6 +67,14 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
   // 1) Getting token and check of it's there
@@ -201,24 +209,28 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no errors
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    // 1) Verify token
-    const decoded = await promisify(jwt.verify)(
-      req.cookies.jwt,
-      process.env.JWT_SECRET
-    );
+    try {
+      // 1) Verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
 
-    // 2) Check if usr still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) return next();
+      // 2) Check if usr still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) return next();
 
-    // 3) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) return next();
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) return next();
 
-    // Thre is a logged in user
-    res.locals.user = currentUser;
-    return next();
+      // Thre is a logged in user
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
   }
   next();
-});
+};
