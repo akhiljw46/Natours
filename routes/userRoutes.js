accbed8338cf39a00c34f39121df4dbc5355565c
Akhil Augustin <akhiljw46@gmail.com>
2023-02-22 07:02:02 +0530

    Multer configured with file filter and storage

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const multer = require('multer');
 // eslint-disable-next-line import/no-useless-path-segments
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
-
-const upload = multer({ dest: 'public/img/users' });
 
 const router = express.Router();
 
@@ -19,7 +16,11 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 
-router.patch('/updateMe', upload.single('photo'), userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.updateMe
+);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.get('/me', userController.getMe, userController.getUser);
