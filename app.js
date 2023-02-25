1dcef7ce9ca3730b0bc358ead16d35bf9103517e
Akhil Augustin <akhiljw46@gmail.com>
2023-02-25 16:53:23 +0530

    Use of compress removed to test deployment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
@@ -7,7 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-const compression = require('compression');
+// const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -73,7 +74,7 @@ app.use(
   })
 );
 
-app.use(compression);
+// app.use(compression);
 
 // Test middleware
 app.use((req, res, next) => {
